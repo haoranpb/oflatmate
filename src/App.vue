af6b30e5ac577e454aecfa18376d25f52196e84c
Sun Haoran <ludanxer@gmail.com>
2020-12-29 11:59:29 +0100

    feat: dev banner

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="flex flex-col h-screen bg-white">
+    <div class="bg-red-300 h-6 text-center">
+      <p>Flatmate is still in development</p>
+    </div>
     <the-heading class="flex container h-16 mx-auto items-center border-b" />
     <router-view class="flex flex-grow container m-auto mt-2" />
   </div>
