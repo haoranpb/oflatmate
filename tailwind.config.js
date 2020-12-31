9e86ea9103433a81e93e39080b05413060afad7d
Sun Haoran <ludanxer@gmail.com>
2020-12-31 19:07:58 +0100

    feat: set up primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./public/*.html', './src/**/*.{vue,ts}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#10B981',
+      },
+    },
   },
   variants: {
     extend: {},
