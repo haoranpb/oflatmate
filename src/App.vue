fa88e086dea785810ff46346c2493f50792dc897
Sun Haoran <ludanxer@gmail.com>
2021-01-09 13:48:26 +0100

    chore: remove wip banner
    it's ugly

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="flex flex-col h-screen bg-white">
-    <div class="bg-red-300 h-6 text-center">
-      <p>Flatmate is still in development</p>
-    </div>
     <router-view />
   </div>
 </template>
