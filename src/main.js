70e489260fb40210a238dca71d5c0bd1e0e36968
Sun Haoran <ludanxer@gmail.com>
2021-01-12 13:49:20 +0100

    style: ignore firebase config spell checking

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'firebase/auth'
 
 const app = createApp(App).use(router)
 
+/* cSpell:disable */
 const firebaseConfig = {
   apiKey: 'AIzaSyAnerucWUMP7sCjJpxsPzkfG51kvWSw84Y',
   authDomain: 'flatmate-3a1eb.firebaseapp.com',
@@ -15,6 +16,7 @@ const firebaseConfig = {
   messagingSenderId: '334498550539',
   appId: '1:334498550539:web:b503b22c5f1fbe46c7c80e',
 }
+/* cSpell:enable */
 firebase.initializeApp(firebaseConfig)
 
 app.config.globalProperties.$firebase = firebase
