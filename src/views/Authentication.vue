96b72539496150624c7d8a447190b683f116eb77
Sun Haoran <ludanxer@gmail.com>
2021-02-04 16:33:39 +0100

    feat: preserve url during login

--- src/views/Authentication.vue
+++ src/views/Authentication.vue
@@ -12,8 +12,14 @@ export default {
   mounted() {
     const ui = new firebaseui.auth.AuthUI(this.$firebase.auth())
 
+    const nextPath = this.$route.redirectedFrom
+      ? this.$route.redirectedFrom.path
+      : '/user'
+
     ui.start('#firebaseui-auth-container', {
-      signInSuccessUrl: '/user',
+      // no auto refresh during login, but getCurrentUser in utils is still needed for user refresh
+      signInFlow: 'popup',
+      signInSuccessUrl: nextPath,
       signInOptions: [
         this.$firebase.auth.EmailAuthProvider.PROVIDER_ID,
         this.$firebase.auth.GoogleAuthProvider.PROVIDER_ID,
