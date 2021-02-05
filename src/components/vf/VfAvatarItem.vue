0c8933efea3f04804b3230296f950d0431d83b5b
Sun Haoran <ludanxer@gmail.com>
2021-02-05 11:37:48 +0100

    feat: default avatar

--- src/components/vf/VfAvatarItem.vue
+++ src/components/vf/VfAvatarItem.vue
@@ -2,17 +2,14 @@
   <img
     class="rounded-full inline-block ring-2 ring-white"
     :class="sizeClass"
-    :src="link"
+    :src="resolveUrl"
   />
 </template>
 
 <script>
 export default {
   props: {
-    link: {
-      type: String,
-      required: true,
-    },
+    link: String,
     large: {
       type: Boolean,
       default: false,
@@ -21,6 +18,9 @@ export default {
   computed: {
     sizeClass() {
       return this.large ? ['h-24', 'w-24'] : ['h-8', 'w-8']
+    },
+    resolveUrl() {
+      return this.link ? this.link : require('@/assets/avatar.svg')
     },
   },
 }
