3ed949eb820a76c032302c763aa25b6767604772
Haoran <ludanxer@icloud.com>
2021-02-21 09:34:45 +0100

    feat: init auth page with template

--- src/views/Authentication.vue
+++ src/views/Authentication.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="authentication">
-    <div id="firebaseui-auth-container"></div>
+    <section class="text-gray-600">
+      <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
+        <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+          <h1 class="font-medium text-3xl text-gray-900">
+            Welcome to Flatmate
+          </h1>
+          <p class="leading-relaxed mt-4">
+            Poke slow-carb mixtape knausgaard, typewriter street art gentrify
+            hammock starladder roathse. Craies vegan tousled etsy austin.
+          </p>
+        </div>
+        <div
+          class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0"
+        >
+          <div id="firebaseui-auth-container"></div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
