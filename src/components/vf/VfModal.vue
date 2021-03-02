123392079b6aa1a058085292be64efece47c368c
Haoran <ludanxer@icloud.com>
2021-03-02 18:10:15 +0100

    feat: set warning as default

--- src/components/vf/VfModal.vue
+++ src/components/vf/VfModal.vue
@@ -24,7 +24,23 @@
               class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full sm:mx-0 sm:h-10 sm:w-10"
               :class="{ 'bg-red-100': warn }"
             >
-              <slot name="icon"></slot>
+              <slot name="icon">
+                <svg
+                  class="h-6 w-6 text-red-600"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                  />
+                </svg>
+              </slot>
             </div>
             <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
               <h3 class="text-lg leading-6 font-medium text-gray-900">
@@ -52,7 +68,7 @@ export default {
   props: {
     warn: {
       type: Boolean,
-      default: false,
+      default: true,
     },
   },
 }
--- src/views/User.vue
+++ src/views/User.vue
@@ -6,23 +6,6 @@
         Are you sure you want to deactivate your account? All of your data will
         be permanently removed. This action cannot be undone.
       </template>
-      <template #icon>
-        <svg
-          class="h-6 w-6 text-red-600"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-          />
-        </svg>
-      </template>
       <template #action>
         <vf-button large danger @click="conformDelete" class="sm:ml-3">
           Deactivate
