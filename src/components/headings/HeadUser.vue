9bbdc9b7a257ca7369ecd5d3946a63dbffbe7193
Haoran <ludanxer@icloud.com>
2021-03-09 09:13:36 +0100

    feat: unify head component ui

--- src/components/headings/HeadUser.vue
+++ src/components/headings/HeadUser.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link
-    class="text-lg text-gray-400 font-medium hover:text-gray-500 leading-normal my-auto"
+    class="text-lg text-gray-400 font-medium hover:text-gray-500 leading-normal my-auto px-3 py-1"
     to="/flat"
   >
     Flats
