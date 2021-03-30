f63f562ee196f5f898464ff09da9be7d084bc951
Haoran <ludanxer@icloud.com>
2021-03-30 08:46:23 +0200

    ui: refine user component design

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="user" class="flex-col">
-    <div v-if="$user" class="flex flex-row w-full m-10 space-x-10">
+  <div id="user" class="flex-col mx-20">
+    <div v-if="$user" class="flex flex-row w-full mb-10 mt-5 space-x-6">
       <vf-avatar-item middle :link="$user.photoURL" />
-      <div class="mt-4">
-        <h2 class="text-2xl font-medium">{{ $user.displayName }}</h2>
-        <p class="text-gray-600">{{ $user.email }}</p>
+      <div class="mt-1">
+        <h2 class="text-xl font-medium">{{ $user.displayName }}</h2>
+        <p class="text-gray-500">{{ $user.email }}</p>
       </div>
     </div>
 
