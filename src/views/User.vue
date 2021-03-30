1857796687fea12a9c5ebbb1f0ddc37e72c88089
Haoran <ludanxer@icloud.com>
2021-03-30 09:50:55 +0200

    feat: init user page layout

--- src/views/User.vue
+++ src/views/User.vue
@@ -8,6 +8,54 @@
       </div>
     </div>
 
-    <router-view />
+    <div class="flex flex-row w-full">
+      <div class="w-80 pr-10">
+        <h3 class="text-2xl font-bold ml-2 mb-4">Settings</h3>
+        <router-link
+          class="setting-link"
+          v-for="item in settings"
+          :to="item.route"
+          :key="item.route"
+        >
+          <i :class="item.icon" class="mr-2 text-gray-500"></i>
+          {{ item.title }}
+        </router-link>
+      </div>
+
+      <div>
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      settings: [
+        {
+          route: 'profile',
+          title: 'Profile',
+          icon: 'far fa-id-card',
+        },
+        {
+          route: 'notification',
+          title: 'Notification',
+          icon: 'fas fa-envelope-open-text',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.setting-link {
+  @apply w-full h-10 leading-10 pl-4 text-lg font-medium text-gray-700 hover:bg-gray-100 inline-block rounded-lg;
+}
+
+.router-link-active {
+  @apply bg-gray-200 !important;
+}
+</style>
